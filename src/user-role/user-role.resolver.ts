2a3ab00d9076f985c307a3384e663f7f5ac59ca2
ashane perera <ashenperera51@yahoo.com>
2021-12-24 02:47:10 +0530

    unit testing

--- src/user-role/user-role.resolver.ts
+++ src/user-role/user-role.resolver.ts
@@ -7,23 +7,28 @@ import { UserRoleService } from './user-role.service';
 @Resolver(() => UserRole)
 export class UserRoleResolver {
   constructor(private userRoleService: UserRoleService) {}
+
+  //Query to get all users
   @Query(() => [UserRole], { name: 'getAllUserRoles' })
   getAll() {
     return this.userRoleService.getAll();
   }
 
+  //mutation to create a new user role
   @Mutation(() => UserRole, { name: 'createUserRole' })
   create(@Args('userroleInput') userrole: UserRoleCreateDto) {
     return this.userRoleService.create(userrole);
   }
 
+  //Mutation to update a exsisting user role
   @Mutation(() => UserRole, { name: 'updateUserRole' })
   updateUserRole(@Args('updateUserRole') UserRoleUpdateDto: UserRoleUpdateDto) {
     return this.userRoleService.update(UserRoleUpdateDto.id, UserRoleUpdateDto);
   }
 
+  //Mutation to remove a userrole from the database
   @Mutation(() => UserRole, { name: 'deleteUserRole' })
-  removeStudent(@Args('id') id: string) {
+  removeUserRole(@Args('id') id: string) {
     return this.userRoleService.remove(id);
   }
 }
--- src/user-role/user-role.service.ts
+++ src/user-role/user-role.service.ts
@@ -12,15 +12,18 @@ export class UserRoleService {
     private userRoleRepository: Repository<UserRole>,
   ) {}
 
+  //this is used to get all rows in the table
   async getAll(): Promise<UserRole[]> {
     return this.userRoleRepository.find();
   }
 
+  //this is used to create a new role in the user role table
   async create(userole: UserRoleCreateDto): Promise<UserRole> {
     const userRole = this.userRoleRepository.create(userole);
     return this.userRoleRepository.save(userRole);
   }
 
+  //this will update the specific row when the id is given
   async update(id: string, userrole: UserRoleUpdateDto): Promise<UserRole> {
     const { roleName } = userrole;
     const user = await this.findOne(id);
@@ -32,10 +35,12 @@ export class UserRoleService {
     return this.userRoleRepository.save(user);
   }
 
+  //this is used to find the specific role by id
   findOne(id: string) {
     return this.userRoleRepository.findOne(id);
   }
 
+  //this will remove the specifi role from the table
   async remove(id: string) {
     let usr = this.findOne(id);
     if (usr) {
